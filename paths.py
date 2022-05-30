7d3231952ca323f3575d6f882c7ccf0625fae42f
Michael Davydov <nixesvfx@gmail.com>
2022-05-30 13:32:09 +0300

    Create logs dir if it's missing (it may be missing from git)

--- paths.py
+++ paths.py
@@ -10,8 +10,9 @@ APP_DIR = os.path.dirname(os.path.realpath(__file__))
 CONFIG_DIR = os.path.join(APP_DIR, "config")
 DATA_DIR = os.path.join(APP_DIR, "data")
 RES_DIR = os.path.join(APP_DIR, "resources")
+LOGS_DIR = os.path.join(APP_DIR, "logs")
 
 
-for d in [CONFIG_DIR, DATA_DIR, RES_DIR]:
+for d in [CONFIG_DIR, DATA_DIR, RES_DIR, LOGS_DIR]:
     if not os.path.exists(d):
         os.makedirs(d)
